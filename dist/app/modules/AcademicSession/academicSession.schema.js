a634b3c4fa4fe5eb78fbc89b858c5a2f734f609d
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-07-26 18:28:26 +0600

    Remove ignored files from repo

--- dist/app/modules/AcademicSession/academicSession.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAcademicSessionSchema = void 0;
-const zod_1 = require("zod");
-exports.createAcademicSessionSchema = zod_1.z.object({
-    name: zod_1.z.string().min(4, "Session name is required"), // e.g. "2020-21"
-});
